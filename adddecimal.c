32b73d1a12600fa37e44436603d104ad464ad904
amritasahu814 <amritasahu814@gmail.com>
2025-08-19 15:13:57 +0530

    c.programing

---
+++ adddecimal.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+      int main (){
+      float a;
+      float b;
+      float c;
+
+      printf ("int value of a");
+      scanf("%f", &a);
+
+      printf("int value of b");
+      scanf("%f", &b);
+
+
+
+      c=a+b;
+    printf ("%f", c);
+}
