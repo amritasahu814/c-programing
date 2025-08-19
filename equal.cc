6651d594a8fbb78941b5d1e54b32755e87700337
amritasahu814 <amritasahu814@gmail.com>
2025-08-19 15:19:13 +0530

    programing

---
+++ equal.cc
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+   int a;
+   int b;
+        if(a>b){
+              printf("a is bigger");
+        }else if (b>a){
+              printf("b is bigger");
+        }else{
+              printf("both are equal");
+
+        }
+}
