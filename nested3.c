5eb9b4895bea83024836912c2295326d3a338605
amritasahu814 <amritasahu814@gmail.com>
2025-08-21 13:11:38 +0530

    nested3.c

---
+++ nested3.c
@@ -0,0 +1,23 @@
+
+int main(){
+    int a;
+    int b;
+    int c;
+
+         printf("enter the angle");
+         scanf("%d",&a);
+         printf("enter the side of angle");
+         scanf("%d",&b);
+         printf("enter the side of angle");
+         scanf("%d",&c);
+
+         if(a+b+c==180){
+                printf("valid ");
+                }else{
+                      printf("no valid");}
+
+
+
+}
+
+
