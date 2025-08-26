cc25deafb1c563199c75aaab26a70109032b444a
amritasahu814 <amritasahu814@gmail.com>
2025-08-26 16:24:54 +0530

    tem.c

---
+++ tem.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+ int main(){
+     float centigrade;
+     float fahrenheit;
+
+
+     printf("enter temperature in centigrade");
+     scanf("%f",&centigrade);
+
+     fahrenheit= (centigrade*9/5)+32;
+
+     printf("temperature in fahrenheit %2f\n", fahrenheit);
+
+
+
+
+
+ }
