12891d4e7c8aae140199cc2dc36e2afa13250bf1
amritasahu814 <amritasahu814@gmail.com>
2025-08-26 16:25:21 +0530

    volume.c

---
+++ volume.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+      float v;
+      float r;
+
+      printf("enter the radius of the sphere");
+      scanf("%f",&r);
+
+      v=(4.0/3.0)*r*r*r;
+
+
+      printf("v of the sphere %2f\n ",v);
+
+
+}
