eb423c3a43930cb8d7035a092a5087eaf8ea6561
amritasahu814 <amritasahu814@gmail.com>
2025-08-26 16:24:19 +0530

    acciment2.c

---
+++ assiment3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int m1;
+    int m2;
+    int m3;
+    int m4;
+    int m5;
+    float percentage;
+
+    printf("enter make 5 subjects");
+    scanf("%d %d %d %d %d",&m1,&m2,&m3,&m4,&m5);
+
+    total=m1+m2+m3+m4+m5;
