e2d84565e5ec671af51305714e2e1fd586c60378
amritasahu814 <amritasahu814@gmail.com>
2025-08-27 11:33:37 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+
+    int a,b,c,sum;
+
+    printf("enter three numbers separated by commas ");
+    scanf("%d,%d,%d", &a,&b,&c);
+
+    sum=a+b+c;
+
+    printf("the sum of three numbers: %d\n", sum);
+
+}
+
